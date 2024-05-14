91b32eac9e51bfe5ab5195c820e310165129fd74
Henok Negatu <anonsurf28@gmail.com>
2024-05-15 00:44:07 +0300

    creating javafx ui

--- student-managment-sys/src/App.java
+++ student-managment-sys/src/App.java
@@ -1,5 +1,28 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("formScene.fxml"));
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("Student Managment");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 }
